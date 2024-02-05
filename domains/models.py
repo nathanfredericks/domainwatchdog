f44af9c01f6fd8f930fb1bbeb3d69cdd5437435b
Nathan Fredericks <nathan@fredericks.dev>
2024-02-05 10:21:59 -0400

    Modify Domain model
    Change description to CharField
    Make WHOIS properties optional
    Add is_autofilled property

--- domains/models.py
+++ domains/models.py
@@ -3,10 +3,21 @@ from django.contrib.auth.models import User
 
 
 class Domain(models.Model):
+    # Domain name (format: example.com)
     name = models.CharField(max_length=255)
-    description = models.TextField()
-    updated_date = models.DateTimeField()
-    creation_date = models.DateTimeField()
-    registry_expiry_date = models.DateTimeField()
-    registrar = models.CharField(max_length=255)
+    # Domain description (optionally set by user for display in dashboard)
+    description = models.CharField(max_length=255, blank=True)
+
+    # WHOIS properties
+    # These properties are autofilled upon first commit.
+    updated_date = models.DateTimeField(blank=True, null=True)
+    creation_date = models.DateTimeField(blank=True, null=True)
+    registry_expiry_date = models.DateTimeField(blank=True, null=True)
+    registrar = models.CharField(max_length=255, blank=True)
+    # If the WHOIS lookup is successful, this property will be switched to true.
+    # If there is an error, the property will stay false and the user will be expected to enter domain properties manually.
+    is_autofilled = models.BooleanField(default=False)
+
+    # Domain owner
+    # This refers to the model's owner in the application's database, **not the registration contact in the WHOIS database.**
     user = models.ForeignKey(User, on_delete=models.CASCADE)
