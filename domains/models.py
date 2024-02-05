fa1c68488759cde56cb2418f3b7a5764aae97cec
Nathan Fredericks <nathan@fredericks.dev>
2024-02-05 09:44:12 -0400

    Create Domain model

--- domains/models.py
+++ domains/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Domain(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    updated_date = models.DateTimeField()
+    creation_date = models.DateTimeField()
+    registry_expiry_date = models.DateTimeField()
+    registrar = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
